ef670ded730b15e826ac6668632818095f7e7242
Thomas63207 <thomasdelange@my.unt.edu>
2026-03-31 21:50:48 -0500

    Buttons now toggle land and water mode

--- Assets/Scripts/Map Editor Scripts/DrawHandler.cs
+++ Assets/Scripts/Map Editor Scripts/DrawHandler.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
 
-public class Drawhandler
+public class DrawHandler : MonoBehaviour
 {
-    
+    public bool landMode = false;
+    public bool waterMode = false;
+
+    public void ToggleLandMode()
+    {
+        landMode = !landMode;
+        waterMode = false; // Ensure water mode is off when land mode is toggled on
+
+        Debug.Log("Land Mode: " + landMode);
+    }
+
+    public void ToggleWaterMode()
+    {
+        waterMode = !waterMode;
+        landMode = false; // Ensure land mode is off when water mode is toggled on
+
+        Debug.Log("Water Mode: " + waterMode);
+    }
 }
